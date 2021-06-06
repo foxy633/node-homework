844bff5b7fa4bc2ed7e89324ba30381f2d54687f
Teslenko Anastasiya <tislenko633@gmail.com>
2021-06-06 17:41:41 +0300

    add-hw-2

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+
+const contactsRouter = require('./routes/api/contacts');
+
+const app = express();
+
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+
+app.use(logger(formatsLogger));
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/contacts', contactsRouter);
+
+app.use((_req, res) => {
+  res.status(404).json({ status: 'Error', code: 404, message: 'Not found' });
+});
+
+app.use((err, _req, res, _next) => {
+  res
+    .status(err.status || 500)
+    .json({ status: 'Error', code: err.status || 500, message: err.message });
+});
+
+module.exports = app;
